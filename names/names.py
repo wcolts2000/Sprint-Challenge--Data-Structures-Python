838e5e9532b0544b411898f9152ba05c0df33035
wcolts2000 <wcolts2000@gmail.com>
2019-07-26 11:13:42 -0700

    accidentally pushed testing code, re pushing correct solution code

--- names/names.py
+++ names/names.py
@@ -19,6 +19,7 @@ duplicates = []
 
 # make names_1 into a set to remove duplicates from on group
 first_group = set(names_1)  # O(1)
+# first_group_list = sorted(names_1)
 # # # print(first_group)
 # # # print(names_2)
 # # # for name in first_group:  # Using a set and the built in list loop in the if shaves off 6 seconds -> time to complete 1.2062842845916748 seconds with 64 duplicates
@@ -26,13 +27,13 @@ first_group = set(names_1)  # O(1)
 # # #         duplicates.append(name)
 
 # # # lets try both as sets
-# second_group = set(names_2)  # O(1)
-# print('firstgroup length: ', len(first_group),
-#       '\nsecondgroup length: ', len(second_group))
-# for name in first_group:  # O(n) # Using 2 set and the built in set loop in the if shaves off even more time -> time to complete 0.005980491638186594 seconds with 64 duplicates
-#     # O(1) ? as per https://wiki.python.org/moin/TimeComplexity this is also O(n)
-#     if name in second_group:
-#         duplicates.append(name)  # O(1)
+second_group = set(names_2)  # O(1)
+print('firstgroup length: ', len(first_group),
+      '\nsecondgroup length: ', len(second_group))
+for name in first_group:  # O(n) # Using 2 set and the built in set loop in the if shaves off even more time -> time to complete 0.005980491638186594 seconds with 64 duplicates
+    # O(1) ? as per https://wiki.python.org/moin/TimeComplexity this is also O(n)
+    if name in second_group:
+        duplicates.append(name)  # O(1)
 
 # AS Lists
 # first_group = list(names_1)
@@ -48,9 +49,9 @@ first_group = set(names_1)  # O(1)
 #     if name in second_group:
 #         duplicates.append(name)
 
-for name in names_2:
-    if binary_search(names_1, name) >= 0:
-        duplicates.append(name)
+# for name in names_2:
+#     if binary_search(first_group_list, name) >= 0:
+#         duplicates.append(name)
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
